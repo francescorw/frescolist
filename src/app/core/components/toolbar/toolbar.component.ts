328041338931cc2835f035f24e4e15fac88e3bcb
Francesco Ozzimo <fr.ozzimo@gmail.com>
2025-04-19 16:39:14 +0200

    feature: replaced the sidenav with the toolbar as the main way to navigate the webapp

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,21 +1,29 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
-    selector: 'frescolist-toolbar',
-    imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
-    templateUrl: './toolbar.component.html',
-    styleUrl: './toolbar.component.scss'
+  selector: 'frescolist-toolbar',
+  imports: [
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
   @Input() appName: string = '';
-  @Output() isMenuOpened: boolean = false;
 
-  menuButtonClicked() : void {
-    this.isMenuOpened = !this.isMenuOpened;
+  constructor(private authService: AuthService) { }
+
+  get isAuthenticated(): boolean {
+    return this.authService.isLoggedIn;
   }
 }
