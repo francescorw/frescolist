68435100b4b308ab61d60fc5de3d0f672de11c12
Francesco Ozzimo <fr.ozzimo@gmail.com>
2025-04-19 02:40:09 +0200

    feat: implement Not Found component with 404 message and styling

--- src/app/core/components/not-found/not-found.component.ts
+++ src/app/core/components/not-found/not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'frescolist-not-found',
-    imports: [],
-    templateUrl: './not-found.component.html',
-    styleUrl: './not-found.component.scss'
+  selector: 'frescolist-not-found',
+  imports: [MatCardModule, MatButtonModule, MatIconModule, RouterLink],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss'
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent { }
