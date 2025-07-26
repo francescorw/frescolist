19a93469be50046dfd62c5c2619fee2c7906d907
Francesco Ozzimo <fr.ozzimo@gmail.com>
2025-07-26 12:43:00 +0200

    chore: upgraded angular and angular-material to v20.1.3

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { SpotifyService } from '../../services/spotify/spotify.service';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'frescolist-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
